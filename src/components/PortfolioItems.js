5e771f7f771f50963710ef5953788a773b7ca0df
Amanda Marshay <amanda.aguilarmiller@gmail.com>
2019-10-16 13:23:43 -0600

    initial commit

---
+++ src/components/PortfolioItems.js
@@ -0,0 +1,173 @@
+import React from "react";
+
+const PortfolioItem = () => {
+  return (
+    <div className="Portfolio-Wrapper">
+      <div className="portfolio-item-wrapper">
+        <div className="photo">
+          <img
+            className="profile-pics"
+            src={require("../victimFive.jpg")}
+            alt="Benprofile"
+          />
+        </div>
+        <div className="custName">Ben</div>
+        <br />
+        <div className="about-user">
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation
+        </div>
+        <button className="see-more">View Profile</button>
+      </div>
+      <div className="portfolio-item-wrapper">
+        <img
+          className="profile-pics"
+          src={require("../victimOne.jpg")}
+          alt="Aimeeprofile"
+        />
+        <div className="photo"></div>
+        <div className="custName">Aimee</div>
+        <br />
+        <div className="about-user">
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation
+        </div>
+
+        <button className="see-more">View Profile</button>
+      </div>
+      <div className="portfolio-item-wrapper">
+        <img
+          className="profile-pics"
+          src={require("../victimTwo.jpg")}
+          alt="Kateprofile"
+        />
+        <div className="photo"></div>
+        <div className="custName">Kate</div>
+
+        <br />
+        <div className="about-user">
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation
+        </div>
+
+        <button className="see-more">View Profile</button>
+      </div>
+      <div className="portfolio-item-wrapper">
+        <img
+          className="profile-pics"
+          src={require("../victimSix.jpg")}
+          alt="Alprofile"
+        />
+        <div className="photo"></div>
+        <div className="custName">Alejandro</div>
+
+        <br />
+        <div className="about-user">
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation
+        </div>
+
+        <button className="see-more">View Profile</button>
+      </div>
+      <div className="portfolio-item-wrapper">
+        <img
+          className="profile-pics"
+          src={require("../victimThree.jpg")}
+          alt="Brennaprofile"
+        />
+        <div className="photo"></div>
+        <div className="custName">Brenna</div>
+
+        <br />
+        <div className="about-user">
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation
+        </div>
+
+        <button className="see-more">View Profile</button>
+      </div>
+      <div className="portfolio-item-wrapper">
+        <img
+          className="profile-pics"
+          src={require("../victimFour.jpg")}
+          alt="Bradprofile"
+        />
+        <div className="photo"></div>
+        <div className="custName">Brad</div>
+
+        <br />
+        <div className="about-user">
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation
+        </div>
+
+        <button className="see-more">View Profile</button>
+      </div>
+      <div className="portfolio-item-wrapper">
+        <img
+          className="profile-pics"
+          src={require("../victimSeven.jpg")}
+          alt="Neilprofile"
+        />
+        <div className="photo"></div>
+        <div className="custName">Neil</div>
+
+        <br />
+        <div className="about-user">
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation
+        </div>
+
+        <button className="see-more">View Profile</button>
+      </div>
+      <div className="portfolio-item-wrapper">
+        <img
+          className="profile-pics"
+          src={require("../VictimEight.jpg")}
+          alt="Ryanprofile"
+        />
+        <div className="photo"></div>
+        <div className="custName">Ryan</div>
+
+        <br />
+        <div className="about-user">
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation
+        </div>
+
+        <button className="see-more">View Profile</button>
+      </div>
+      <div className="portfolio-item-wrapper">
+        <img
+          className="profile-pics"
+          src={require("../victimNine.jpg")}
+          alt="Neilprofile"
+        />
+        <div className="photo"></div>
+        <div className="custName">Joe</div>
+
+        <br />
+        <div className="about-user">
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation
+        </div>
+
+        <button className="see-more">View Profile</button>
+        <footer>....</footer>
+        <footer>....</footer>
+        <footer>....</footer>
+      </div>
+    </div>
+  );
+};
+
+export default PortfolioItem;
---
+++ src/components/top-links.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+
+const TopLinks = () => {
+  return (
+    <div className="top-links-wrapper">
+      <div className="image-wrapper">
+        <img src={require("../whiteLogo.png")} />
+      </div>
+      <div className="links-wrapper">
+        <div>
+          <a href className="link">
+            Home
+          </a>
+        </div>
+
+        <div>
+          <a href className="link">
+            Join
+          </a>
+        </div>
+
+        <div>
+          <a href className="link">
+            About
+          </a>
+        </div>
+
+        <div>
+          <a href className="link">
+            Contact
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TopLinks;
