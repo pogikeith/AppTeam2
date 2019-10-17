6914e4f999c556db92a52647f20915b2be16450b
Amanda Marshay <amanda.aguilarmiller@gmail.com>
2019-10-17 14:10:53 -0600

    updated styling, connected to backend

--- src/components/PortfolioItems.js
+++ src/components/PortfolioItems.js
@@ -1,276 +1,51 @@
-import React from "react";
+import React, { Component } from "react";
 
-const PortfolioItem = () => {
-  return (
-    <div className="Portfolio-Wrapper">
-      <div className="portfolio-item-wrapper">
-        <div className="photo">
-          <img
-            className="profile-pics"
-            src={require("../victimFive.jpg")}
-            alt="Benprofile"
-          />
-        </div>
-        <div className="custName">Ben</div>
-        <br />
-        <div className="about-user">
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation
-        </div>
-        <button className="see-more">View Profile</button>
-      </div>
-      <div className="portfolio-item-wrapper">
-        <div className="photo">
-          <img
-            className="profile-pics"
-            src={require("../victimTen.jpg")}
-            alt="Benprofile"
-          />
-        </div>
-        <div className="custName">Alex</div>
-        <br />
-        <div className="about-user">
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation
-        </div>
-        <button className="see-more">View Profile</button>
-      </div>{" "}
-      <div className="portfolio-item-wrapper">
-        <div className="photo">
-          <img
-            className="profile-pics"
-            src={require("../victimFive.jpg")}
-            alt="Benprofile"
-          />
-        </div>
-        <div className="custName">Ben</div>
-        <br />
-        <div className="about-user">
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation
-        </div>
-        <button className="see-more">View Profile</button>
-      </div>
-      <div className="portfolio-item-wrapper">
-        <div className="photo">
-          <img
-            className="profile-pics"
-            src={require("../victimFive.jpg")}
-            alt="Benprofile"
-          />
-        </div>
-        <div className="custName">Ben</div>
-        <br />
-        <div className="about-user">
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation
-        </div>
-        <button className="see-more">View Profile</button>
-      </div>
-      <div className="portfolio-item-wrapper">
-        <div className="photo">
-          <img
-            className="profile-pics"
-            src={require("../victimFive.jpg")}
-            alt="Benprofile"
-          />
-        </div>
-        <div className="custName">Ben</div>
-        <br />
-        <div className="about-user">
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation
-        </div>
-        <button className="see-more">View Profile</button>
-      </div>
-      <div className="portfolio-item-wrapper">
-        <div className="photo">
-          <img
-            className="profile-pics"
-            src={require("../victimFive.jpg")}
-            alt="Benprofile"
-          />
-        </div>
-        <div className="custName">Ben</div>
-        <br />
-        <div className="about-user">
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation
-        </div>
-        <button className="see-more">View Profile</button>
-      </div>
-      <div className="portfolio-item-wrapper">
-        <div className="photo">
-          <img
-            className="profile-pics"
-            src={require("../victimFive.jpg")}
-            alt="Benprofile"
-          />
-        </div>
-        <div className="custName">Ben</div>
-        <br />
-        <div className="about-user">
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation
-        </div>
-        <button className="see-more">View Profile</button>
-      </div>
-      <div className="portfolio-item-wrapper">
-        <img
-          className="profile-pics"
-          src={require("../victimOne.jpg")}
-          alt="Aimeeprofile"
-        />
-        <div className="photo"></div>
-        <div className="custName">Aimee</div>
-        <br />
-        <div className="about-user">
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation
-        </div>
+export default class PortfolioItem extends Component {
+  constructor() {
+    super();
 
-        <button className="see-more">View Profile</button>
-      </div>
-      <div className="portfolio-item-wrapper">
-        <img
-          className="profile-pics"
-          src={require("../victimTwo.jpg")}
-          alt="Kateprofile"
-        />
-        <div className="photo"></div>
-        <div className="custName">Kate</div>
+    this.state = {
+      data: []
+    };
+  }
 
-        <br />
-        <div className="about-user">
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation
+  componentDidMount() {
+    fetch("https://reporter-fake-dating-site-db.herokuapp.com/users")
+      .then(response => response.json())
+      .then(results => this.setState({ data: results }))
+
+      .catch(error => {
+        console.log("error", error);
+      });
+  }
+
+  renderItems = () => {
+    return this.state.data.map(item => {
+      return (
+        <div className="portfolio-item-wrapper">
+          <div className="photo">
+            <img
+              className="profile-pics"
+              src={item.profile_picture}
+              alt="Benprofile"
+            />
+          </div>
+          <div className="custName">
+            <h2>
+              {item.first_name} {item.last_name}
+            </h2>
+          </div>
+          <br />
+          <div className="about-user">
+            <h3>{item.username}</h3>
+          </div>
+          <button className="see-more">View Profile</button>
         </div>
+      );
+    });
+  };
 
-        <button className="see-more">View Profile</button>
-      </div>
-      <div className="portfolio-item-wrapper">
-        <img
-          className="profile-pics"
-          src={require("../victimSix.jpg")}
-          alt="Alprofile"
-        />
-        <div className="photo"></div>
-        <div className="custName">Alejandro</div>
-
-        <br />
-        <div className="about-user">
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation
-        </div>
-
-        <button className="see-more">View Profile</button>
-      </div>
-      <div className="portfolio-item-wrapper">
-        <img
-          className="profile-pics"
-          src={require("../victimThree.jpg")}
-          alt="Brennaprofile"
-        />
-        <div className="photo"></div>
-        <div className="custName">Brenna</div>
-
-        <br />
-        <div className="about-user">
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation
-        </div>
-
-        <button className="see-more">View Profile</button>
-      </div>
-      <div className="portfolio-item-wrapper">
-        <img
-          className="profile-pics"
-          src={require("../victimFour.jpg")}
-          alt="Bradprofile"
-        />
-        <div className="photo"></div>
-        <div className="custName">Brad</div>
-
-        <br />
-        <div className="about-user">
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation
-        </div>
-
-        <button className="see-more">View Profile</button>
-      </div>
-      <div className="portfolio-item-wrapper">
-        <img
-          className="profile-pics"
-          src={require("../victimSeven.jpg")}
-          alt="Neilprofile"
-        />
-        <div className="photo"></div>
-        <div className="custName">Neil</div>
-
-        <br />
-        <div className="about-user">
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation
-        </div>
-
-        <button className="see-more">View Profile</button>
-      </div>
-      <div className="portfolio-item-wrapper">
-        <img
-          className="profile-pics"
-          src={require("../VictimEight.jpg")}
-          alt="Ryanprofile"
-        />
-        <div className="photo"></div>
-        <div className="custName">Ryan</div>
-
-        <br />
-        <div className="about-user">
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation
-        </div>
-
-        <button className="see-more">View Profile</button>
-      </div>
-      <div className="portfolio-item-wrapper">
-        <img
-          className="profile-pics"
-          src={require("../victimNine.jpg")}
-          alt="Neilprofile"
-        />
-        <div className="photo"></div>
-        <div className="custName">Joe</div>
-
-        <br />
-        <div className="about-user">
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation
-        </div>
-
-        <button className="see-more">View Profile</button>
-
-        <footer>....</footer>
-        <footer>....</footer>
-        <footer>....</footer>
-      </div>
-    </div>
-  );
-};
-
-export default PortfolioItem;
+  render() {
+    return <div className="Portfolio-Wrapper">{this.renderItems()}</div>;
+  }
+}
